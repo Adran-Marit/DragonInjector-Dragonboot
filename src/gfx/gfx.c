5b21fb2b14a69124e2d817fee10334a024a04a59
pixel-stuck <mathmcclintic@gmail.com>
2019-08-17 00:10:05 -0400

    small fixes

--- src/gfx/gfx.c
+++ src/gfx/gfx.c
@@ -54,16 +54,16 @@ void draw_payload_screen(u32 **framebuf, u32 x, u32 y, u32 payload_num)
 
     u8 char_img_lut[] = {16, 14, 3, 3, 11, 16, 16, 10, 14, 7, 7, 22, 24, 7, 15, 21, 5, 11, 5, 24, 8, 21, 22, 7, 15, 21, 5, 24};
     u8 char_img_number_lut[] = {24, 20, 4, 2, 17. 1, 16, 23, 0};
-    u8 chars[25][483];
+    u32 chars[25][483];
     memcpy(chars, decomp_char_img, decomp_char_img_size);
 
     for(int i = 0; i < sizeof(char_img_lut); i++)
     {
-        draw_image_32bpp(framebuf, chars[char_img_lut[i]], x, y, 23, 21);
+        draw_image_32bpp(framebuf, &chars[char_img_lut[i]][0], x, y, 23, 21);
         x += 23;
     }
 
-    draw_image_32bpp(framebuf, chars_white[char_img_number_lut[payload_num]], x, y, 23, 21);
+    draw_image_32bpp(framebuf, &chars[char_img_number_lut[payload_num]][0], x, y, 23, 21);
     draw_image_32bpp(framebuf, logo_white_img, 50, 100, 178, 128);
 
     set_active_framebuffer(*framebuf);
